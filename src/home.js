d8c9a35f08066cfd7d5f4f2b8827e655a3d16d4f
Brittney Wong <brittneywong33@gmail.com>
2016-06-30 23:36:45 -0700

    Add some style; add build folder

--- src/home.js
+++ src/home.js
@@ -5,7 +5,7 @@ class Home extends React.Component {
   render() {
     return (
       <div> 
-        <h1>brittney wong</h1>
+        <h1 className="home-title">brittney wong</h1>
         <h3>sup</h3>
         <Bio />
       </div>
